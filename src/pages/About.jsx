c82c871c608c8d9f070de7482de1b79647ee3cde
Racknar0 <racknarow1@gmail.com>
2022-04-05 13:10:45 -0500

    conditionals

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,16 +5,19 @@ const About = () => {
     const { user } = useContext(UserContext);
 
     return (
-        <div>
+        <div className='container text-center mt-5'>
             <h1>About</h1>
 
-
-             {user && (
-                <h2>
-                    {user.data.first_name} {user.data.last_name}
-                </h2>
-            )} 
+            {!user ? <h2 className='text-danger'>Please Login</h2> : (
+              <div>
+                <h3>Name: {user.data.first_name} {user.data.last_name}</h3>
+                <h3>Email: {user.data.email}</h3>
+                <img src={user.data.avatar} alt={user.data.id} width='100px' height='100px' />
+              </div>
+            ) }
         </div>
+
+        
     );
 };
 
